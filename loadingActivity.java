fb34bb26343c8655b620335c59d577722523f6df
originalminji <32849046+originalminji@users.noreply.github.com>
2019-08-08 00:13:16 +0900

    Add files via upload

---
+++ loadingActivity.java
@@ -0,0 +1,32 @@
+package com.example.kusteau;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class loadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+
+}
